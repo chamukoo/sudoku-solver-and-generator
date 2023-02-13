ae2ac6ae70c318f094ba6ea7189867c8d9993d26
Lee Anne <leeangeles2@gmail.com>
2023-02-14 00:40:25 +0800

    Solving sudoku using backtracking

--- solver.py
+++ solver.py
@@ -23,3 +23,27 @@ def isSafeToMove(grid, row, col, guess):
             
     return True
 
+
+# Solve sudoku puzzle using backtracking
+def solveSudoku(grid, row, col):
+    if col == 9:
+        if row == 8:
+            return True
+        row += 1
+        col = 0
+
+    if grid[row][col] > 0:
+        return solveSudoku(grid, row, col + 1)
+    
+    for guess in range(1, 10):
+        # CHeck if this guess is valid
+        if isSafeToMove(grid, row, col, guess):
+            # If valid, then place that guess on the board
+            grid[row][col] = guess
+            # Then recurse this grid to call our function
+            if solveSudoku(grid, row, col + 1):
+                return True
+
+        # If not valid, then we need to backtrack
+        # and try a new guess/number 
+        grid[row][col] = -1
