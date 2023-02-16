6463305b7d31b08c755db567d12248d891682a5d
Lee Anne <leeangeles2@gmail.com>
2023-02-17 02:08:13 +0800

    Create Unsolved Sudoku

--- gui.py
+++ gui.py
@@ -1,17 +1,18 @@
 from tkinter import *
 from solver import Solver, Generator, Puzzle
+import time
 
 
 window = Tk()
 window.title("Sudoku Solver")
-window.geometry("370x480")
+window.geometry("370x580")
 
 
-# This is where the user inputs are stored
+''' Making blank list to store all the entry widget cells '''
 cells = {}
 
 
-# Checking the validity of numbers
+''' Checking the validity of numbers '''
 def isValid(P):
     out = (P.isdigit() or P=="") and len(P) < 2
     return out
@@ -19,19 +20,19 @@ def isValid(P):
 reg = window.register(isValid)
 
 
-# Drawing the 3x3 Grid
+''' Drawing the 3x3 Grid '''
 def draw3x3Grid(row, column, bgcolor):
     for i in range(3):
         for j in range(3):
             num = Entry(window, width=5, bg=bgcolor, fg='black', 
                               font=('Arial', 10, 'bold'), justify='center', cursor="plus", 
                               validate='key', validatecommand=(reg, '%P'))
-            num.grid(row=row+i+1, column=column+j+1, sticky='nsew', 
+            num.grid(row = row + i + 1, column = column + j + 1, sticky='nsew', 
                            padx=1, pady=1, ipady=5)
-            cells[(row+i+1, column+j+1)] = num
+            cells[(row + i + 1, column + j + 1)] = num
 
 
-# Drawing a 9x9 Grid 
+''' Drawing a 9x9 Grid '''
 def draw9x9Grid():
     color = "light yellow"
     for row in range(1, 10, 3):
@@ -43,7 +44,7 @@ def draw9x9Grid():
                 color = "light yellow"
 
 
-# Update the values in Cells
+''' Update the values in Cells '''
 def update(sudoku):
     solve = Solver(sudoku)
     if solve != "solve":
@@ -56,23 +57,39 @@ def update(sudoku):
         errorLabel.configure(text='Sudoku is UNSOLVABLE!')
 
 
-# Generate a solved puzzle (called by new button)
+'''
+Sudoku is in the correct format and is completly random
+Generates a completed sudoku 
+(called by new button)
+'''
 def generate(sudoku):
     new = Generator(sudoku)
     if new != "generate":
-        for r in range(2, 11):
-            for c in range(1, 10):
-                cells[(r, c)].delete(0, END)
-                cells[(r, c)].insert(0, new[r - 2][c - 1])
+        for rows in range(2, 11):
+            for cols in range(1, 10):
+                cells[(rows, cols)].delete(0, END)
+                cells[(rows, cols)].insert(0, new[rows - 2][cols - 1])
+                
 
-        
+
+''' Create an unsolved sudoku puzzle '''
+def scramble(sudoku):
+    startTime = time.time()
+    new = Puzzle(sudoku)
+    if new != "puzzle":
+        for rows in range(2, 11):
+            for cols in range(1, 10):
+                cells[(rows, cols)].delete(0, END)
+                cells[(rows, cols)].insert(0, new[rows - 2][cols - 1])
+                
 
 
-# Function to solve the sudoku (called by solve button)
+''' 
+Function to solve the sudoku puzzle
+(called by solve button) 
+'''
 def solve():
-    # making blank list to store all the entry widget cells 
     board= []
-
     errorLabel.configure(text="")
     solvedLabel.configure(text="")
 
@@ -91,26 +108,40 @@ def solve():
     update(board)
 
 
-# Function to generate solved sudoku puzzle (called by generate button)
+'''
+Function to generate solved sudoku puzzle 
+(called by generate button)
+'''
 def regenerate():
+    errorLabel.configure(text="")
+    solvedLabel.configure(text="")
+
     grid = [[0 for r in range(9)] for c in range(9)]
-            
+                
     for r in range(9):
         for c in range(9):
             grid[r][c] = 0
-
+    
     generate(grid)
 
 
-def puzzle():
-    grid = [[0 for r in range(9)] for c in range(9)]
-            
-    for r in range(9):
-        for c in range(9):
-            grid[r][c] = 0
+'''
+Function to create new usolved sudoku puzzle 
+(called by generate button)
+'''
+def new():
+    errorLabel.configure(text="")
+    solvedLabel.configure(text="")
 
-   
-# Function to clear board (called by clear button)
+    grid = [[0 for r in range(9)] for c in range(9)]
+    
+    scramble(grid)
+
+
+'''
+Function to clear the board/cells 
+(called by clear button)
+'''
 def clear():
     errorLabel.configure(text="")
     solvedLabel.configure(text="")
@@ -120,35 +151,47 @@ def clear():
             cells[(row, col)].delete(0, END)
 
 
-# Labels
-label = Label(window, text="Welcome to Sudoku Solver!",
+
+''' Labels '''
+
+label = Label(window, text="Welcome to Sudoku Generator and Solver!",
               font=("Helvetica", 12), fg="black", pady = 10)
 label.grid(row=0, column=1,columnspan=10)
 
-# For unsolvavble sudoku puzzle
+''' For unsolvavble sudoku puzzle '''
 errorLabel = Label(window, text="", fg="red", font=("Arial", 16))
-errorLabel.grid(row=20, column=1, columnspan=10, pady=20)
+errorLabel.grid(row=20, column=1, columnspan=10, pady=10)
 
-# For solvable sudoku puzzle
+''' For solvable sudoku puzzle '''
 solvedLabel = Label(window, text="", fg="green", font=("Arial", 16))
-solvedLabel.grid(row=20, column=1, columnspan=10, pady=20)
+solvedLabel.grid(row=20, column=1, columnspan=10, pady=10)
 
-# Buttons for solving the board
-getBtn = Button(window, command=solve, text='Solve', width=10,
+
+''' Buttons '''
+
+''' For creating unsolved puzzle '''
+newBtn = Button(window, command=new, text='New Puzzle', width=12,
                 activebackground='light cyan', font=('Arial', 10, 'bold'))
-getBtn.grid(row=50, column=0, columnspan=5, pady=10)
+newBtn.grid(row=30, column=1, columnspan=5, pady=10)
 
-# Buttons for clearing the board
-clearBtn = Button(window, command=clear, text='Clear', width=10,
+''' For solving the board '''
+solveBtn = Button(window, command=solve, text='Solve', width=12, 
+                activebackground='light cyan', font=('Arial', 10, 'bold'))
+solveBtn.grid(row=30, column=5, columnspan=5, pady=10)
+
+''' For generating new sudoku puzzle '''
+generateBtn = Button(window, command=regenerate, text='Generate', width=12,
+                activebackground='light cyan', font=('Arial', 10, 'bold'))
+generateBtn.grid(row=50, column=1, columnspan=5, pady=10)
+
+''' For clearing the board '''
+clearBtn = Button(window, command=clear, text='Clear', width=12, 
                   activebackground='light cyan', font=('Arial', 10, 'bold'))
-clearBtn.grid(row=50, column=3, columnspan=5, pady=10)
-
-# Buttons for generating new sudoku puzzle 
-newBtn = Button(window, command=regenerate, text='Generate', width=10,
-                activebackground='light cyan', font=('Arial', 10, 'bold'))
-newBtn.grid(row=50, column=6, columnspan=7, pady=10)
+clearBtn.grid(row=50, column=5, columnspan=5, pady=10)
 
 
-# Main Loop
+
+''' Main Loop '''
+
 draw9x9Grid()
 window.mainloop()
--- solver.py
+++ solver.py
@@ -1,23 +1,23 @@
 import random
 
+''' 
+Checking whether the guess at the row/column of  
+the grid is a valid guess and is safe to move 
+'''
+def checkValidity(grid, row, col, guess):
 
-# Checking whether the guess at the row/column of the grid  
-# is a valid guess and is safe to move
-def isSafeToMove(grid, row, col, guess):
-
-    # Check if same number occurs in rows
+    ''' Check if same number occurs in rows '''
     for r in range(9):
         if grid[row][r] == guess:
             return False
         
-    # Check if same number occurs in columns
+    ''' Check if same number occurs in column '''
     for c in range(9):
         if grid[c][col] == guess:
             return False
         
-    # Check if same number exist in 3x3 Grid
+    ''' Check if same number exist in 3x3 Grid '''
     startRow, startCol = (row // 3) * 3, (col // 3) * 3
-
     for r in range(startRow, startRow + 3):
         for c in range(startCol, startCol + 3):
             if grid[r][c] == guess:
@@ -26,7 +26,7 @@ def isSafeToMove(grid, row, col, guess):
     return True
 
 
-# Solve sudoku puzzle using backtracking
+''' Solve sudoku puzzle using backtracking '''
 def solveSudoku(grid, row, col):
     if col == 9:
         if row == 8:
@@ -34,30 +34,37 @@ def solveSudoku(grid, row, col):
         row += 1
         col = 0
 
-    # checking if the cell is filled with non-zero number
+    ''' Checking if the cell is filled with non-zero number '''
     if grid[row][col] > 0:
 
-        # starting the sudoku solver again, 
-        # recursion call is done to move to the next column
+        ''' 
+        Starting the sudoku solver again, then 
+        recursion call is done to move to the next column 
+        '''
         return solveSudoku(grid, row, col + 1)
     
     for guess in range(1, 10):
-        # CHeck if this guess is valid
-        if isSafeToMove(grid, row, col, guess):
-            # If valid, then place that guess on the board
+
+        ''' Check if the guess is valid'''
+        if checkValidity(grid, row, col, guess):
+
+            ''' If valid, then place that guess on the board '''
             grid[row][col] = guess
-            # Then recurse this grid to call our function
+
+            ''' Then recurse this grid to call our function '''
             if solveSudoku(grid, row, col + 1):
                 return True
 
-        # If not valid, then we need to backtrack
-        # and try a new guess/number, reset to zero
+        '''
+        If not valid, then we need to backtrack
+        and try a new guess/number, reset to zero
+        '''
         grid[row][col] = 0
 
     return False
 
 
-# Generate solved sudoku puzzle
+''' Generate completed sudoku puzzle '''
 def generateSudoku(grid, row, col):
         num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         if col == 9:
@@ -66,31 +73,58 @@ def generateSudoku(grid, row, col):
             row += 1
             col = 0
 
-        # skipping cell if the cell has already a number
+        ''' Skipping grid if the grid has already a number '''
         if grid[row][col] != 0:
             if generateSudoku(grid, row, (col + 1)):
                 return True
-        # solving for the cell if the cell is empty
+                
+        # Solving for the grid if the grid is empty
         else:
-            # Randomize number
+            ''' Randomize number '''
             random.shuffle(num)
             for guess in num:
-                # Check if the number is valid
-                if isSafeToMove(grid, row, col, guess):
-                    # If valid, then place that guess on the board
+
+                ''' Check if the guess is valid '''
+                if checkValidity(grid, row, col, guess):
+
+                    ''' If valid, then place that guess on the grid '''
                     grid[row][col] = guess
-                    # Then recurse this grid to call our function
+                    
+                    ''' Then recurse this grid to call our function '''
                     if generateSudoku(grid, row, (col + 1)):
                         return True
                     
-               # If not valid, then we need to backtrack
-               # then guess another number
-                grid[row][col] = guess
+                ''' 
+                If not valid, then we need to backtrack, 
+                and try a new number, reset to zero 
+                '''
+                grid[row][col] = 0
 
         return False
 
 
-# Solver
+''' Create New Puzzle '''
+def createSudoku(grid, row, col):
+
+    ''' The range here is the amount of numbers in the grid '''
+    for r in range(1, 17):
+
+        ''' Choose random numbers '''
+        row = random.randrange(9)
+        col = random.randrange(9)
+        guess = random.randrange(1,10)
+
+        while not checkValidity(grid, row, col, guess) or grid[row][col] != 0:
+            row = random.randrange(9)
+            col = random.randrange(9)
+            guess = random.randrange(1,10)
+
+        grid[row][col] = guess
+
+    return grid
+
+
+''' Solver '''
 def Solver(grid):
     if solveSudoku(grid, 0, 0):
         return grid
@@ -98,9 +132,17 @@ def Solver(grid):
         return "solve"
         
 
-# Generator
+''' Generator '''
 def Generator(grid):
     if generateSudoku(grid, 0, 0):
         return grid
     else:
         return "generate"
+
+
+''' Puzzle '''
+def Puzzle(grid):
+    if createSudoku(grid, 0, 0):
+        return grid
+    else:
+        return "puzzle"
